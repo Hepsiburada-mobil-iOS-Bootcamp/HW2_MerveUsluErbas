1abdd4ced5b5777a5f1d2b2b69e87b9d71d8990f
Merve Uslu Erbaş <uslumerve33@gmail.com>
2021-10-02 23:10:23 +0300

    Develop branch is created.

--- PermissionProjectMerveStyle2/ViewController.swift
+++ PermissionProjectMerveStyle2/ViewController.swift
@@ -9,42 +9,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    private var photoCameraButtons : PhotoCameraButtons!
-
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        addButtonFunc()
-    }
-
-
-
-
-
-
-
-
-    private func addButtonFunc () {
-      photoCameraButtons = PhotoCameraButtons()
-        photoCameraButtons.delegate = self
-        photoCameraButtons.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(photoCameraButtons)
-        NSLayoutConstraint.activate([
-            photoCameraButtons.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            photoCameraButtons.centerYAnchor.constraint(equalTo: view.centerYAnchor)
-        ])
-
-    }
-
-
-}
-
-extension ViewController: HomeViewDelegate{
-    func cameraPhotoButtonSelected(sender: UIButton) {
-        let permissionView = PermissionViewController()
-        permissionView.buttonType = (sender.titleLabel?.text)!
-        permissionView.view.backgroundColor = UIColor.clear
-               self.present(permissionView, animated: true)
     }
 }
+
+
+
+
+
 
